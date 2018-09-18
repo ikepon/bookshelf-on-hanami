eaa21bd19520f2ab0ead138ac2d46351b37de899
ikepon <tatsuyanoheya@gmail.com>
2018-09-18 23:29:13 +0900

    Use route path

--- apps/web/controllers/books/create.rb
+++ apps/web/controllers/books/create.rb
@@ -14,7 +14,7 @@ module Web::Controllers::Books
     def call(params)
       if params.valid?
         @book = BookRepository.new.create(params[:book])
-        redirect_to '/books'
+        redirect_to routes.books_path
       else
         self.status = 422
       end
