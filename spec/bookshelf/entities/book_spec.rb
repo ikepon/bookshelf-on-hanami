ff46cdf29706cd8e61b08c12bf49fb1d1ec9cf46
ikepon <tatsuyanoheya@gmail.com>
2018-09-18 17:31:38 +0900

    Create Book entity

--- spec/bookshelf/entities/book_spec.rb
+++ spec/bookshelf/entities/book_spec.rb
@@ -1,3 +1,8 @@
-RSpec.describe Book, type: :entity do
-  # place your tests here
+require_relative '../../spec_helper'
+
+describe Book, type: :entity do
+  it 'can be initialized with attributes' do
+    book = Book.new(title: 'Refactoring')
+    expect(book.title).to eq('Refactoring')
+  end
 end
